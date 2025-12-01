6210c3b45b921998d6939401b46cd48244539fbd
Charbie <eve.charbonneau.1@umontreal.ca>
2025-12-01 13:39:10 +0100

    Got numerical values, but there are nans in the sympy expression :(

--- bicyle_rider_model.py
+++ bicyle_rider_model.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Nov 25 11:08:14 2025
 
-@author: Jules
+@author: Jules + EveCharbie
 """
 
 # =============================================================================
@@ -16,10 +16,12 @@ Created on Tue Nov 25 11:08:14 2025
 # pip install symbrim
 # pip install bicycleparameters
 # pip install symmeplot
+# conda install -c conda-forge casadi
 
 
 import warnings
 from IPython.display import display
+import numpy as np
 
 import sympy as sm
 import sympy.physics.mechanics as me
@@ -28,6 +30,7 @@ import symbrim as sb
 from symbrim.bicycle import RigidRearFrameMoore, WhippleBicycleMoore
 from symbrim.brim import SideLeanSeatSpringDamper
 from symbrim.rider import PinElbowTorque, SphericalShoulderTorque
+from sympy.utilities.lambdify import lambdify
 
 
 bicycle = sb.WhippleBicycle("bike_v1_0")
@@ -122,30 +125,30 @@ print(missing_symbols)
 
 #%% Simulation
 
-import numpy as np
-from simulator import Simulator
+if False:
+    from simulator import Simulator
 
-simu = Simulator(system)
+    simu = Simulator(system)
 
-simu.constants = constants
-simu.initial_conditions = {
-    **{xi: 0.0 for xi in system.q.col_join(system.u)},
-    bicycle.q[4]: 0.314,  # Initial guess rear frame pitch.
-    bicycle.u[5]: -3.0 / constants[bicycle.rear_wheel.radius],  # Rear wheel angular velocity.
-}
-roll_rate_idx = len(system.q) + system.u[:].index(bicycle.u[3])
-max_roll_rate, max_torque = 0.2, 10
-simu.inputs = {
-    disturbance: lambda t, x: (30 + 30 * t) * np.sin(t * 2 * np.pi),
-    steer_torque: lambda t, x: -max_torque * max(-1, min(x[roll_rate_idx] / max_roll_rate, 1)),
-}
+    simu.constants = constants
+    simu.initial_conditions = {
+        **{xi: 0.0 for xi in system.q.col_join(system.u)},
+        bicycle.q[4]: 0.314,  # Initial guess rear frame pitch.
+        bicycle.u[5]: -3.0 / constants[bicycle.rear_wheel.radius],  # Rear wheel angular velocity.
+    }
+    roll_rate_idx = len(system.q) + system.u[:].index(bicycle.u[3])
+    max_roll_rate, max_torque = 0.2, 10
+    simu.inputs = {
+        disturbance: lambda t, x: (30 + 30 * t) * np.sin(t * 2 * np.pi),
+        steer_torque: lambda t, x: -max_torque * max(-1, min(x[roll_rate_idx] / max_roll_rate, 1)),
+    }
 
-simu.initialize()
-print('Initial Conditions:')
-simu.initial_conditions
+    simu.initialize()
+    print('Initial Conditions:')
+    simu.initial_conditions
 
 
-simu.solve([0,5], solver="solve_ivp")
+    simu.solve([0,5], solver="solve_ivp")
 
 #%% Visualization
 
@@ -194,7 +197,6 @@ if False:
 #%% Conversion
 
 
-from sympy_to_casadi import sympy_to_casadi_2
 from sympy import lambdify, symbols
 from sympy.physics.mechanics import *
 import casadi as cas
@@ -217,18 +219,16 @@ def eval_num_full(
     disturbance = dynamicsymbols('disturbance')
     
     n = np.shape(M_m)[0]
-    
+
+    # This fails with "The requested array has an inhomogeneous shape after 2 dimensions. The detected shape was (16, 16) + inhomogeneous part."
     # f_M_m = lambdify(
     #     ( _x, _p, steer_torque, disturbance),
     #      M_m.reshape(n, n), cse=True)
-    
+    # M_m_num = f_M_m(x, _p_vals, tau, distu)
     
     f_F_m = lambdify(
         ( _x, _p, steer_torque, disturbance),
          F_m.reshape(1, n), cse=True)
-    
-    # M_m_num = f_M_m(x, _p_vals, tau, distu)
-    
     F_m_num = f_F_m(x, _p_vals, tau, distu)
         
     return F_m_num
@@ -238,10 +238,25 @@ def sympy_to_casadi_function(
         function_name: str,
         sympy_expr: sm.matrices.dense.MutableDenseMatrix,
         sympy_var: tuple[sm.matrices.immutable.ImmutableDenseMatrix],
-        casadi_var: list[cas.MX],
         constants: dict[str, float],
 ):
-    from sympy.utilities.lambdify import lambdify
+    def safe_blockcat(matrix):
+        """
+        Make sure every element of a matrix is a CasADi expression, so that blockcat can work.
+        """
+        rows = []
+        for row in matrix:
+            new_row = []
+            for elem in row:
+                # replace raw ints/floats with CasADi constants
+                if type(elem) == sm.core.numbers.NaN:
+                    elem = cas.SX(0)  # TODO: see why there are NaNs !
+                elif isinstance(elem, (int, float)):
+                    elem = cas.SX(elem)
+                new_row.append(elem)
+            rows.append(new_row)
+        return cas.blockcat(rows)
+
 
     variable_list = [
         cas.SX.sym("bike_v1_0_q1", 1, 1),
@@ -288,50 +303,53 @@ def sympy_to_casadi_function(
     disturbance = variable_list[17]
 
 
-    casadi_mapping = {'ImmutableDenseMatrix': cas.blockcat,
-               'MutableDenseMatrix': cas.blockcat,
-               'Abs': cas.fabs,
-               'sin': cas.sin,
-               'cos': cas.cos,
-               'tan': cas.tan,
-               'asin': cas.asin,
-               'acos': cas.acos,
-               'atan': cas.atan,
-               'sinh': cas.sinh,
-               'cosh': cas.cosh,
-               'tanh': cas.tanh,
-               'asinh': cas.asinh,
-               'acosh': cas.acosh,
-               'atanh': cas.atanh,
-               'exp': cas.exp,
-               'log': cas.log,
-               'sqrt': cas.sqrt,
-              'bike_v1_0_q1(t)': bike_v1_0_q1,
-              'bike_v1_0_q2(t)': bike_v1_0_q2,
-              'bike_v1_0_q3(t)': bike_v1_0_q3,
-              'bike_v1_0_q4(t)': bike_v1_0_q4,
-              'bike_v1_0_q6(t)': bike_v1_0_q6,
-              'bike_v1_0_q7(t)': bike_v1_0_q7,
-              'bike_v1_0_q8(t)': bike_v1_0_q8,
-              'bike_v1_0_q5(t)': bike_v1_0_q5,
-              'bike_v1_0_u4(t)': bike_v1_0_u4,
-              'bike_v1_0_u6(t)': bike_v1_0_u6,
-              'bike_v1_0_u7(t)': bike_v1_0_u7,
-              'bike_v1_0_u1(t)': bike_v1_0_u1,
-              'bike_v1_0_u2(t)': bike_v1_0_u2,
-              'bike_v1_0_u3(t)': bike_v1_0_u3,
-              'bike_v1_0_u5(t)': bike_v1_0_u5,
-              'bike_v1_0_u8(t)': bike_v1_0_u8,
-              'steer_torque': steer_torque,
-              'disturbance': disturbance,
-               }
+    casadi_mapping = {
+       'ImmutableDenseMatrix': safe_blockcat,
+       'MutableDenseMatrix': safe_blockcat,
+       # 'ImmutableDenseMatrix': cas.blockcat,
+       # 'MutableDenseMatrix': cas.blockcat,
+       'Abs': cas.fabs,
+       'sin': cas.sin,
+       'cos': cas.cos,
+       'tan': cas.tan,
+       'asin': cas.asin,
+       'acos': cas.acos,
+       'atan': cas.atan,
+       'sinh': cas.sinh,
+       'cosh': cas.cosh,
+       'tanh': cas.tanh,
+       'asinh': cas.asinh,
+       'acosh': cas.acosh,
+       'atanh': cas.atanh,
+       'exp': cas.exp,
+       'log': cas.log,
+       'sqrt': cas.sqrt,
+      'bike_v1_0_q1(t)': bike_v1_0_q1,
+      'bike_v1_0_q2(t)': bike_v1_0_q2,
+      'bike_v1_0_q3(t)': bike_v1_0_q3,
+      'bike_v1_0_q4(t)': bike_v1_0_q4,
+      'bike_v1_0_q6(t)': bike_v1_0_q6,
+      'bike_v1_0_q7(t)': bike_v1_0_q7,
+      'bike_v1_0_q8(t)': bike_v1_0_q8,
+      'bike_v1_0_q5(t)': bike_v1_0_q5,
+      'bike_v1_0_u4(t)': bike_v1_0_u4,
+      'bike_v1_0_u6(t)': bike_v1_0_u6,
+      'bike_v1_0_u7(t)': bike_v1_0_u7,
+      'bike_v1_0_u1(t)': bike_v1_0_u1,
+      'bike_v1_0_u2(t)': bike_v1_0_u2,
+      'bike_v1_0_u3(t)': bike_v1_0_u3,
+      'bike_v1_0_u5(t)': bike_v1_0_u5,
+      'bike_v1_0_u8(t)': bike_v1_0_u8,
+      'steer_torque': steer_torque,
+      'disturbance': disturbance,
+       }
     # Add constants
     for name, value in constants.items():
         casadi_mapping[str(name)] = float(value)
 
     f = lambdify(sympy_var, sympy_expr, modules=casadi_mapping)
     f(x_list, steer_torque, disturbance)
-    casadi_func = cas.Function(function_name, casadi_var, [f(*casadi_var)])
+    casadi_func = cas.Function(function_name, variable_list, [f(x_list, steer_torque, disturbance)])
     # ERROR: casadi expressions not iterable by design
 
     return casadi_func
@@ -352,40 +370,30 @@ from the sympy and casadi expression then check if we get the same values.
 """
 
 _x = system.q.col_join(system.u)
-x = np.ones(np.shape(_x))
+x = np.zeros(np.shape(_x))
 tau = np.ones(1)
 distu = np.ones(1)
+value_list = [0] * np.shape(_x)[0] + [1] + [1]
 
 print(eval_num_full(system, x, 1, 1))
 
 # Convertir M_m et F_m en CasADi
-# M_casadi = sympy_to_casadi_2(M_d)
-# F_casadi = sympy_to_casadi_2(F_d)
-
-x_sym = cas.MX.sym('x', len(_x))
-tau_sym = cas.MX.sym('tau')
-distu_sym = cas.MX.sym('distu')
-
 f_M_func = sympy_to_casadi_function(
     "M_d",
     M_d,
     (_x, steer_torque, disturbance),
-    [x_sym, tau_sym, distu_sym],
     constants,
 )
 f_F_func = sympy_to_casadi_function(
     "F_d",
     F_d,
     (_x, steer_torque, disturbance),
-    [x_sym, tau_sym, distu_sym],
     constants,
 )
-# f_M = ca.Function('f_M', [x_sym, p_sym, tau_sym, distu_sym], [M_casadi])
-# f_F = cas.Function('f_F', [x_sym, tau_sym, distu_sym], [cas.vec(F_casadi)])
 
-#Erreur à ce niveau là
-# M_num = np.array(f_M(x, _p_vals, tau, distu)).astype(float)
-print(np.array(f_M_func(x, tau, distu)).astype(float))
+# Erreur à ce niveau là
+print(np.array(f_M_func(*value_list)).astype(float))
+print(np.array(f_F_func(*value_list)).astype(float))
 
 
 
